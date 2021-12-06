7b600bbfb331cb67cea850ec668fe165f605fcbe
ShihHeng <happy.890927410@gmail.com>
2021-12-07 01:10:54 +0800

    ShihHeng 稍微做修正

--- Backend/threading/Threading.py
+++ Backend/threading/Threading.py
@@ -1,21 +1,22 @@
-import threading
+from threading import Thread, Event
 
 # target: function name
 # name: threading name
 # args: function parameter
 
-class Job(threading.Thread):
+class Job(Thread):
     def __init__(self, class_name, func_name, args):
         super(Job, self).__init__(  name = class_name,
                                     target = func_name,
-                                    args = args,
-                                    daemon = True)
-        self.__running = threading.Event()
-        self.__unlock = threading.Event()
-        self.__pause = threading.Event()
+                                    args = args)
+        self.__running = Event()
+        self.__unlock = Event()
+        self.__pause = Event()
         self.__running.clear()
         self.__unlock.set()
         self.__pause.clear()
+
+        print("Thread '", class_name, "' setup successfully.", sep='')
     
     def run(self):
         if self.__unlock.is_set():
@@ -25,6 +26,12 @@ class Job(threading.Thread):
             self.__unlock.wait()
             self.__running.set()
             super(Job, self).run()
+
+    def check_info(self):
+        name = super(Job, self).getName()
+        print("Thread ", name, " running is ", True if self.__running.is_set() else False, sep='')
+        print("Thread ", name, " unlock is ", True if self.__unlock.is_set() else False, sep='')
+        print("Thread ", name, " pause is ", True if self.__pause.is_set() else False, sep='')
 
     def is_run(self):
         return True if self.__running.is_set() else False
