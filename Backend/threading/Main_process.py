9c8a5b35db061748f3becbe540b6556922e379f3
ShihHeng <happy.890927410@gmail.com>
2021-12-07 01:11:57 +0800

    ShihHeng 新增動態導入feature & 測試多執行緒功能

--- Backend/threading/Main_process.py
+++ Backend/threading/Main_process.py
@@ -1,14 +1,17 @@
-from _typeshed import HasFileno
+import os
 import queue
-import threading
+import time
 
 from Threading import Job
+from importlib import import_module
+
+feature_path = './feature'
 
 class Main():
     def __init__(self):
         self.threads = []
         self.data_que = queue.Queue()
-        self.__
+        self.feature_list = []
 
     def add_thread(self, class_name, func_name, args):
         self.threads.append(Job(class_name, func_name, args))
@@ -16,10 +19,54 @@ class Main():
     def send(self, data):
         self.data_que.put(data)
 
+def reciprocal(num):
+    for n in range(num, 0, -1):
+        print(n, 'seconds')
+        time.sleep(1)
+
 if __name__ == "__main__":
+    # defination main process
     main = Main()
     
-    while True:
+    # initial feature class
+    feature_list = os.listdir(feature_path)
+    for file in feature_list:
+        full_filename = os.path.basename(feature_path + '/' + file)
+        filename = os.path.splitext(full_filename)
+
+        # if not a python file skip
+        if filename[1] != '.py':
+            continue
+        else:
+            feature = filename[0]
+        
+        try:
+            # import feature module
+            module = import_module(feature_path.replace('./', '') + '.' + feature)
+            try:
+                # from feature module get class object
+                class_entity = getattr(module, feature)
+                feature_obj = {
+                    'class': class_entity,
+                    'name': feature
+                }
+                main.feature_list.append(feature_obj)
+            except:
+                continue
+        except:
+            continue
+    
+    main.add_thread('main', reciprocal, (20,))
+
+    main.threads[0].check_info()
+
+    main.threads[0].start()
+
+    reciprocal(10)
+
+    main.threads[0].join()
+
+    while False:
         for thread in main.threads:
             if thread.is_run():
                 continue
