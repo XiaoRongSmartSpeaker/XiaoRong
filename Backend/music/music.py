28f21bdae8dc94878db453e884fad2daff61b9fe
alice0520 <alice900531@gmail.com>
2021-12-13 15:18:09 +0800

    sss

--- Backend/music/music.py
+++ Backend/music/music.py
@@ -1,52 +1,90 @@
 import pafy
 import vlc
-import youtube_dl
+#import youtube_dl
+import yt_dlp as youtube_dl
 #import webbrowser
 from googleapiclient.discovery import build
+import urllib.request
 
 API_KEY = 'AIzaSyDYvK29Z74AqL3lGK8c3tgR0RsFygKyJkU'
 
-def pafy_video(videoId):
-    url = 'https://www.youtube.com/watch?v={0}'.format(videoId)
-    print(url)
-#    webbrowser.open(url)
-    ydl_opts = {"--no-check-certificate": True}
-    play_video = pafy.new(url, ydl_opts)
-    best = play_video.getbestaudio()
-#    media = vlc.MediaPlayer(best.url)
-#    media.play()
-    play_url = best.url
-    Instance = vlc.Instance()
-    player = Instance.media_player_new()
-    Media = Instance.media_new(play_url)
-    Media.get_mrl()
-    player.set_media(Media)
-    player.play()
+class MusicStreaming():
 
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+    def __init__(self, text):
+        self.target = text
 
-text = '周杰倫的給我一首歌的時間'
+    def pafy_video(self, videoId):
+        url = 'https://www.youtube.com/watch?v={0}'.format(videoId)
+        print(url)
+    #    webbrowser.open(url)
+    #    ydl_opts = {"--no-check-certificate": True}
+    #    play_video = pafy.new(url, ydl_opts)
+        play_video = pafy.new(url)
+        best = play_video.getbestaudio()
+    #    media = vlc.MediaPlayer(best.url)
+    #    media.play()
+        play_url = best.url
+        Instance = vlc.Instance()
+        player = Instance.media_player_new()
+        
+        code = urllib.request.urlopen(url).getcode()
+        if str(code).startswith('2') or str(code).startswith('3'):
+            print('Stream is working')
+        else:
+            print('Stream is dead')
+        
+        Media = Instance.media_new(play_url)
+        Media.get_mrl()
+        player.set_media(Media)
+        events = player.event_manager()
+        player.play()
+        print(events)
+        
+        good_states = ["State.Playing", "State.NothingSpecial", "State.Opening"]
+        while str(player.get_state()) in good_states:
+            print('Stream is working. Current state = {}'.format(player.get_state()))
 
-request = youtube.search().list(
-        part = 'id, snippet',
-        maxResults = 3,
-        order = 'viewCount',
-        q = text
-    )
+        print('Stream is not working. Current state = {}'.format(player.get_state()))
+        player.stop()
 
-response = request.execute()
+    def play_music(self):
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
 
-print(response)
+        request = youtube.search().list(
+                part = 'id, snippet',
+                maxResults = 3,
+                order = 'viewCount',
+                q = self.target
+            )
 
-videos = []
+        response = request.execute()
 
-for search_result in response.get('items', []):
-    if search_result['id']['kind'] == 'youtube#video':
-        videos.append('%s' % (search_result['id']['videoId']))
+        print(response)
 
-if videos:
-    print('Videos:{0}'.format(videos))
-    pafy_video(videos[0])
+        videos = []
+
+        for search_result in response.get('items', []):
+            if search_result['id']['kind'] == 'youtube#video':
+                videos.append('%s' % (search_result['id']['videoId']))
+
+        if videos:
+            print('Videos:{0}'.format(videos))
+            self.pafy_video(videos[0])
+        
+        
+if __name__ == "__main__":
+    text = 'chungha killing me'
+    music = MusicStreaming(text)
+    music.play_music()
+    
+#Instance = vlc.Instance()
+#player = Instance.media_player_new()
+#Media = Instance.media_new('test.mp3')
+#Media.get_mrl()
+#player.set_media(Media)
+#player.play()
+#
+#print(player.play())
 
 #import mpv
 #import yt_dlp
