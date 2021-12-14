0018c69532c74ac8eaaa1027eab1bdcf33f68ddc
Sophie <sophielien09@gamil>
2021-12-15 01:29:04 +0800

    feat: modify countdown __init__ and setCountdown

--- Backend/time/countdown.py
+++ Backend/time/countdown.py
@@ -3,24 +3,30 @@ import playsound
 import os
 class countdown:
 	def __init__(self):
-		True
-	def setCountdown(self, h: int, m: int, s: int):
-		totalSeconds = s + m * 60 + h * 3600
-		if totalSeconds < 0 or totalSeconds > 7200:
-			return False
-
-		while totalSeconds:
-			m, s = divmod(totalSeconds, 60)
+		self.totoalSeconds = 0
+		while self.totalSeconds:
+			m, s = divmod(self.totalSeconds, 60)
 			h, m = divmod(m, 60)
 			timeFormat = '{:02d}:{:02d}:{:02d}'.format(h, m, s)
 			print(timeFormat)
 			time.sleep(1)
-			totalSeconds -= 1
+			self.totalSeconds -= 1
 		# print('Time\'s up!')
 		audio_file = 'sound.mp3'
 		playsound.playsound(audio_file, True)                                      
 		return True
 
+	def setCountdown(self, h: int, m: int, s: int):
+		if h <= 0 or m <= 0 or s <= 0:
+			return False
+		totalSeconds = s + m * 60 + h * 3600
+		if 0 <= totalSeconds <= 7200:
+			self.totalSeconds = totalSeconds
+			return True
+		return False
+	def stopCountdown(self):
+		pass
+		 
 if __name__ == "__main__":
     a = countdown()
     if a.setCountdown(0, 0, 1) == False:
