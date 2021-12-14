792bdd6d9f63ee557bce7192b01a71e8a7753dce
Sophie <sophielien09@gamil>
2021-12-15 01:29:04 +0800

    feat: modify countdown __init__ and setCountdown

--- Backend/time/countdown.py
+++ Backend/time/countdown.py
@@ -9,7 +9,8 @@ sys.path.append(os.path.abspath(scriptpath))
 from Threading import Job
 class countdown():
 	def __init__(self):
-		self.totalSeconds = 0                               
+		self.totalSeconds = 0  
+		self.audio_file = 'sound.mp3'                             
 		return
 
 	def set_countdown(self, h: int, m: int, s: int) -> bool:
@@ -25,17 +26,23 @@ class countdown():
 	def start_countdown(self):
 		while self.totalSeconds:
 			m, s = divmod(self.totalSeconds, 60)
-
 			h, m = divmod(m, 60)
 			timeFormat = '{:02d}:{:02d}:{:02d}'.format(h, m, s)
 			print(timeFormat)
 			time.sleep(1)
 			self.totalSeconds -= 1
-		# print('Time\'s up!')
-		audio_file = 'sound.mp3'
-		playsound.playsound(audio_file, True) 
+		print('Time\'s up!')
+		playsound.playsound(self.audio_file, True) 
 		return True
 	def stop_countdown(self):
+		if h <= 0 or m <= 0 or s <= 0:
+			return False
+		totalSeconds = s + m * 60 + h * 3600
+		if 0 <= totalSeconds <= 7200:
+			self.totalSeconds = totalSeconds
+			return True
+		return False
+	def stopCountdown(self):
 		pass
 		 
 if __name__ == "__main__":
