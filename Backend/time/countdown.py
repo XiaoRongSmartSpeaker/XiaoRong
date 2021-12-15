8867cb3b27cf7de59b50e0b82363b52d8a0a70ed
Sophie8909 <sophielien09@gmail.com>
2021-12-15 23:14:45 +0800

    feat: add func start_countdown and support input h:m:s for test

--- Backend/time/countdown.py
+++ Backend/time/countdown.py
@@ -1,9 +1,28 @@
 import time
 import playsound
+import sys
 import os
-class countdown:
+scriptpath = "../"
+
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
+from Threading import Job
+class countdown():
 	def __init__(self):
-		self.totoalSeconds = 0
+		self.totalSeconds = 0                               
+		return
+
+	def set_countdown(self, h: int, m: int, s: int):
+		if h < 0 or m < 0 or s < 0:
+			return False
+		totalSeconds = s + m * 60 + h * 3600
+		if 0 <= totalSeconds <= 7200:
+			self.totalSeconds = totalSeconds
+			self.start_countdown()
+			return True
+		
+		return False
+	def start_countdown(self):
 		while self.totalSeconds:
 			m, s = divmod(self.totalSeconds, 60)
 			h, m = divmod(m, 60)
@@ -13,22 +32,13 @@ class countdown:
 			self.totalSeconds -= 1
 		# print('Time\'s up!')
 		audio_file = 'sound.mp3'
-		playsound.playsound(audio_file, True)                                      
+		playsound.playsound(audio_file, True) 
 		return True
-
-	def setCountdown(self, h: int, m: int, s: int):
-		if h <= 0 or m <= 0 or s <= 0:
-			return False
-		totalSeconds = s + m * 60 + h * 3600
-		if 0 <= totalSeconds <= 7200:
-			self.totalSeconds = totalSeconds
-			return True
-		return False
-	def stopCountdown(self):
+	def stop_countdown(self):
 		pass
 		 
 if __name__ == "__main__":
-    a = countdown()
-    if a.setCountdown(0, 0, 1) == False:
-    	print('error time!')
-    
+	a = countdown()
+	h, m, s = map(int,input("輸入到計時時間（格式：hh:mm:ss）").split(":"))
+	if a.set_countdown(h, m, s) == False:
+		print('error time!')
