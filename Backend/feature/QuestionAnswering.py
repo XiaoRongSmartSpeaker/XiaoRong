f3a4dd392896b27d369f08aabd9363d58bc2c1fd
Willy-Chen <willychenwii@gmail.com>
2021-12-21 02:58:08 +0000

    fix: 1. removed unused codes 2. add comments to every function, include args and return values 3. combine get_answer into google_search 4. check for connection error

--- Backend/feature/QuestionAnswering.py
+++ Backend/feature/QuestionAnswering.py
@@ -2,62 +2,56 @@ import requests, urllib
 from requests_html import HTML
 from requests_html  import HTMLSession
 
+class ConnectoinError():
+    pass
+
 class QuestionAnswering():
     def __init__(self):
         self.query = ""
         self.url = "https://www.google.com/search?q="
 
     def get_source(self, url):
-        """Return the source code for the provided URL. 
+        """
+        Return the source code for the provided URL. 
         Args: 
             url (string): URL of the page to scrape.
         Returns:
             response (object): HTTP response object from requests_html. 
         """
         try:
+            #open session for url page
             session = HTMLSession()
             response = session.get(url)
             return response
 
         except requests.exceptions.RequestException as e:
-            print(e)
+            raise ConnectoinError
+            return None
 
-
-    def scrape_google(self):
-
-        query = urllib.parse.quote_plus(self.query)
-        response = self.get_source("https://www.google.com/search?q=" + query)
-
-        links = list(response.html.absolute_links)
-        google_domains = ('https://www.google.', 
-                        'https://google.', 
-                        'https://webcache.googleusercontent.', 
-                        'http://webcache.googleusercontent.', 
-                        'https://policies.google.',
-                        'https://support.google.',
-                        'https://maps.google.')
-
-        for url in links[:]:
-            if url.startswith(google_domains):
-                links.remove(url)
-
-        return links
 
     def get_results(self):
-        
+        """
+        Return html object from url and query
+        """
         query = urllib.parse.quote_plus(self.query)
         response = self.get_source(self.url+self.query)
         
         return response
 
     def parse_results(self, response):
-        
+        """
+        Return content from html body
+        Args:
+            response(html response): html body of a scraped page
+        Returns:
+            output(list): content parsed from html  
+        """
+        #css identifier to find result from Google result page
         css_identifier_result = ".tF2Cxc"
         css_identifier_title = "h3"
         css_identifier_link = ".yuRUbf a"
         css_identifier_text = ".IsZvec"
         css_identifier_diet = ".hgKElc"
-        # css_identifier_similar = ".Wt5Tfe span"
         css_identifier_ecom = ".PZPZlf span"
         css_identifier_exchange = ".b1hJbf"
         css_identifier_calculate = ".vUGUtc, .qv3Wpe"
@@ -66,11 +60,12 @@ class QuestionAnswering():
         css_identifier_internet_diet = ".kp-blk"
         css_identifier_time = ".card-section.sL6Rbf"
 
-        results = response.html.find(css_identifier_result)
-
+        #store css find result
         output = []
+
+        #get content from html body by css identifier
+        results = response.html.find(css_identifier_result)
         diet = response.html.find(css_identifier_diet, first=True)
-        # similar = response.html.find(css_identifier_similar, first=False)
         ecom = response.html.find(css_identifier_ecom, first=False)
         exchange = response.html.find(css_identifier_exchange, first=True)
         calculate = response.html.find(css_identifier_calculate, first=False)
@@ -84,16 +79,6 @@ class QuestionAnswering():
         else:
             output.append({'diet':None})
 
-        # if similar:
-        #     output.append({'similar':""})
-        #     print(similar)
-        #     for it in similar:
-        #         if 'class' in it.attrs and it.attrs['class'][0] == 'hgKElc':
-        #             print(it.attrs)
-        #             output[-1]['similar'] += it.html + ' '
-        # else:
-        #     output.append({'similar':None})
-
         if time:
             output.append({'time':time.text})
         else:
@@ -102,11 +87,6 @@ class QuestionAnswering():
         if ecom:
             output.append({'ecom':""})
             for it in ecom:
-                # if 'class' in it.attrs:
-                #     print( it.attrs['class'][0])
-                #     print(it.attrs['class'][0] == 'a4vfUd')
-                # if 'class' in it.attrs and it.attrs['class'][0]=='jBBUv':
-                #     print(it.attrs['class'])
                 if 'jscontroller' in it.attrs and it.attrs['jscontroller']=='B82lxb':
                     output[-1]['ecom']+=it.text+' '
                 if 'jsname' in it.attrs and it.attrs['jsname']=='qRSVye':
@@ -136,8 +116,6 @@ class QuestionAnswering():
             output[-1]['weather'] += weather[0].find(".VQF4g")[0].text.replace('\n', ' ') + ' '
             output[-1]['weather'] += weather[0].find(".wtsRwe")[0].text.replace('\n', ' ')[:-5]
             for it in weather[0].find('span'):
-                # if 'id' in it.attrs:
-                #     print(it.attrs['id'])
                 if 'id' in it.attrs and it.attrs['id'] == 'wob_tm':
                     output[-1]['weather'] += f' {it.text}°C'
                 if 'id' in it.attrs and it.attrs['id'] == 'wob_ttm':
@@ -147,7 +125,6 @@ class QuestionAnswering():
 
         if support:
             output.append({'support':[]})
-            # print(support[0].find(".kno-rdesc")[0].text)
             for it in support:
                 tmp = it.find('a')
                 if tmp:
@@ -177,6 +154,13 @@ class QuestionAnswering():
         return output
 
     def get_answer(self, results):
+        """
+        Select answer from parsed output, if no answer found, return not found string.
+        Args:
+            results(list): list of parsed result
+        Returns:
+            ans(string): answer string to respond
+        """
         flag = True
         ans = ''
         for result in results:
@@ -192,18 +176,28 @@ class QuestionAnswering():
         
         ans = (ans.strip())
         if ans == '':
-            print(f"沒有查到「{search.query}」的相關資料")
+            ans = f"沒有查到「{search.query}」的相關資料"
 
         return ans
 
     def google_search(self, query):
+        """
+        Main function to scrape and parse result and return answer string.
+        Args:
+            query(string): query string to search for
+        Return:
+            ans(string): result of query.
+        """
         self.query = query
         response = self.get_results()
-        return self.parse_results(response)
+        if response is None:
+            ans = f"沒有查到「{search.query}」的相關資料"
+            return ans
+        results =  self.parse_results(response)
+        return self.get_answer(results)
 
 if __name__ == '__main__':  
     search = QuestionAnswering()
-    results = search.google_search(str(input("請輸入問題:")))
-    ans = search.get_answer(results)
+    ans = search.google_search(str(input("請輸入問題:")))
     
     print(ans)
