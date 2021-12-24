0ed9a865568cd6a8d60791f2ffa8450b65f03471
ShihHeng <happy.890927410@gmail.com>
2021-12-24 22:47:26 +0800

    新增暫停判斷跟等待機制

--- Backend/threading/Threading.py
+++ Backend/threading/Threading.py
@@ -14,22 +14,23 @@ class Job(Thread):
         self.name = name
         self.func = func.__name__
         self.args = args
+        self.result = None
         
         # threading control parameter
         self.__main_porc = main
         self.__running = Event()
         self.__unlock = Event()
-        self.__pause = Event()
+        self.__resume = Event()
         self.__running.clear()
         self.__unlock.set()
-        self.__pause.clear()
+        self.__resume.set()
 
         print("Thread '", name, "' setup successfully.", sep='')
     
     def reset(self) -> None:
         self.__running.clear()
         self.__unlock.set()
-        self.__pause.clear()
+        self.__resume.set()
     
     def run(self) -> None:
         if self.__unlock.is_set():
@@ -43,19 +44,25 @@ class Job(Thread):
     def add_thread(self, func_info) -> None:
         self.__main_porc.add_thread(func_info)
 
-    def check_info(self):
+    def check_info(self) -> None:
         print("Thread ", self.name, " running is ", True if self.__running.is_set() else False, sep='')
         print("Thread ", self.name, " unlock is ", True if self.__unlock.is_set() else False, sep='')
-        print("Thread ", self.name, " pause is ", True if self.__pause.is_set() else False, sep='')
+        print("Thread ", self.name, " pause is ", True if self.__resume.is_set() else False, sep='')
 
     def is_run(self) -> bool:
         return True if self.__running.is_set() else False
 
+    def is_pause(self) -> bool:
+        return False if self.__resume.is_set() else True
+    
+    def wait_for_exec(self) -> None:
+        self.__resume.is_set()
+
     def pause(self) -> None:
-        self.__pause.set()
+        self.__resume.clear()
 
     def resume(self) -> None:
-        self.__pause.clear()
+        self.__resume.set()
 
     def lock(self) -> None:
         self.__unlock.clear()
