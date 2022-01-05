3cde51dba8408aa0c1c8193571c0746ec62a973b
eoleedi <eoleedimin@gmail.com>
2022-01-05 21:41:26 +0800

    fix: fix module issue caused by autopep8

--- Backend/feature/__init__.py
+++ Backend/feature/__init__.py
@@ -1,8 +1,8 @@
-from .SpeechToText import SpeechToText
-from .TextToSpeech import TextToSpeech
-from .Extract import Extract
-from .Bluetooth import Bluetooth
 import sys
 import os
 # add feature path to sys.path
 sys.path.append(os.path.dirname(__file__))
+from .SpeechToText import SpeechToText
+from .TextToSpeech import TextToSpeech
+from .Extract import Extract
+from .Bluetooth import Bluetooth
