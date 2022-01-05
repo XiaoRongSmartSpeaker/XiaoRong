a5a7c8712e12de964edbf28efd305c4b74a99191
eoleedi <eoleedimin@gmail.com>
2022-01-05 21:54:07 +0800

    fix: autopep8 not messing up with __init__.py

--- Backend/feature/__init__.py
+++ Backend/feature/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import os
 # add feature path to sys.path
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(__file__))  # noqa: E402
 from .SpeechToText import SpeechToText
 from .TextToSpeech import TextToSpeech
 from .Extract import Extract
