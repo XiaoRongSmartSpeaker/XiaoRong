297e0f09cd36243a09606fe5c419771b80e68af8
joseph89108 <joseph89108@github.com>
2022-01-09 07:40:11 +0800

    edit wifi_scan

---
+++ Backend/wifi/wifi_scan.py
@@ -0,0 +1,34 @@
+import os
+import re
+import json
+
+class WiFi:
+
+        def __init__(self):
+                pass
+
+        def scan(self):
+                list_result = []
+
+                scan = os.popen("sudo iwlist wlan1 scan","r",-1).read()
+
+                mac = re.findall('Address: ([A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2})\s',scan)
+                ssid = re.findall('ESSID:\"(.*?)\"',scan)
+                freq = re.findall('Frequency:(.*?)\n',scan)
+                Encryption = re.findall('key:(.*?)\s',scan)
+                Security = re.findall('',scan)
+                Quality = re.findall('Quality=(\d{1,3}/100)',scan)
+                Signal = re.findall('Signal level=(\d{1,3}/100)',scan)
+
+                for i in range(0,len(mac)):
+                        dict_result = {}
+                        dict_result['MAC'] = mac[i]
+                        dict_result['SSID']  = ssid[i]
+                        dict_result['Frequency'] = freq[i]
+                        dict_result['Encryption'] = Encryption[i]
+                        dict_result['Quality'] = Quality[i]
+                        dict_result['Signal'] = Signal[i]
+                        list_result.append(dict_result)
+
+                result = json.dumps(list_result, indent = 4)
+                return result
