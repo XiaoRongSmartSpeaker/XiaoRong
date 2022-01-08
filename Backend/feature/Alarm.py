d6870044159792e084c32a59b143cb220b457166
Sophie8909 <sophielien09@gmail.com>
2022-01-08 14:35:42 +0800

    feat: done Alarm

--- Backend/feature/Alarm.py
+++ Backend/feature/Alarm.py
@@ -1,9 +1,11 @@
 from logging import Manager
 import time
-
-import playsound
+import multiprocessing
+from playsound import playsound
 import sys
 import json
+
+import threading
 try:
 	import logger
 	logger = logger.get_logger(__name__)
@@ -23,19 +25,26 @@ class Alarm():
 	def __init__(self):
 		self.alarmList = []
 		self.audioFile = 'sound.mp3'  
-		self.threadHandler = None                        
+		self.threadHandler = None  
+		self.alarmListFile = 'AlarmList.json'
+		self.isPlayingAudio = False  
+		self.playingAudio = None  
+		self.audioSec = 150              
 		return
 	def import_thread(self, thread):
 		self.threadHandler = thread
 		return
 
-	def get_alarm_list():
+	def get_alarm_list(self):
+		jsonContent = {}
 		with open( "AlarmList.json", encoding="utf-8") as f:
 			jsonContent = f.read()
 		jsonContent = json.loads(jsonContent)
-		# 取得 json "BODY" 欄位
-		AlarmList = jsonContent["AlarmList"]  
-	# TODO
+		# 取得 json "AlarmList" 欄位
+		try:
+			self.alarmList = jsonContent["AlarmList"]  
+		except:
+			elf.alarmList = []
 	def set_alarm(self, day: int, h: int, m: int):
 		if not 1 <= day <= 7:
 			logger.debug('Error day')
@@ -46,14 +55,52 @@ class Alarm():
 		elif not 0 <= m < 60:
 			logger.debug('Error minute')
 			return False
-		
+		self.alarmList.append({"day": day, "hour": h, "minute": m})
+		self.save_alarm_list()
 		return
-	# TODO
+	def save_alarm_list(self):
+		with open(self.alarmListFile, mode="w", encoding="utf-8") as f:
+			json.dump({"AlarmList":self.alarmList}, f, ensure_ascii=False)
+		return None
+	def start_alarm(self):
+		while True:
+			t = time.localtime()
+			now = {"day": t.tm_wday + 1, "hour": t.tm_hour, "minute": t.tm_min}
+			for setTime in self.alarmList:
+				if now == setTime:
+					logger.info('Alarm time')
+					self.isPlayingAudio = True
+					self.play_ring()
+					while self.isPlayingAudio and self.audioSec:
+						self.audioSec -= 1
+						time.sleep(1)
+					self.audioSec = 150
+					self.isPlayingAudio = False
+					logger.info('end of ring')
+					self.playingAudio.terminate()
+					break
+			time.sleep(1)
+
+	def play_ring(self):
+		self.playingAudio = multiprocessing.Process(target=playsound, args=(self.audioFile,))
+		self.playingAudio.start()
 	def main(self):
-		pass
-		
+		self.get_alarm_list()
+		# self.start_alarm()
+		t = threading.Thread(target=self.start_alarm)
+		t.start()
+		# self.threadHandler.add_thread({
+		# 	'class': 'Alarm',
+		# 	'func': 'start_alarm',
+		# })
+	def stop_ringing(self):
+		self.isPlayingAudio = False
+		return
 if __name__ == "__main__":
-    a = Alarm()
-    day, h, m = map(int, input("輸入鬧鐘時間（格式：星期(1-7):hh:mm）").split(":"))
-    if a.set_alarm(day, h, m) == False:
-        print('error time!')
+	a = Alarm()
+	day, h, m = map(int, input("輸入鬧鐘時間（格式：星期(1-7):hh:mm）").split(":"))
+	a.main()
+	if a.set_alarm(day, h, m) == False:
+		print('error time!')
+	time.sleep(30)
+	a.stop_ringing()
