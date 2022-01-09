07ce969496e3dc2e796bd90b833a257fdbc643c6
Sophie8909 <sophielien09@gmail.com>
2022-01-09 14:15:25 +0800

    feat: worldtime add thread

--- Backend/feature/WorldTime.py
+++ Backend/feature/WorldTime.py
@@ -1,10 +1,27 @@
 import pytz
 from datetime import datetime
-     
+import sys
+try:
+    import logger
+    logger = logger.get_logger(__name__)
+except ModuleNotFoundError:
+    import logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
 class WorldTime():
     def __init__(self):
         self.tzList = []
-
+        self.threadHandler = None 
+    def import_thread(self, thread):
+		self.threadHandler = thread
+		return
     def get_time_zone_list(self):
         if self.tzList == []:
             self.tzList = pytz.all_timezones
@@ -15,7 +32,19 @@ class WorldTime():
         for tz in self.tzList:
             if place.casefold() in tz.casefold():
                 tzId = pytz.timezone(tz)
-                print(datetime.now(tzId).strftime('%Y:%m:%d %H:%M:%S %Z %z'))
+                tzTime = datetime.now(tzId).strftime('%Y:%m:%d %H:%M:%S %Z %z')
+                logger.debug(tzTime)
+                self.threadHandler.add_thread({
+                    'class': 'TextToSpeech',
+                    'func': 'text_to_voice',
+                    'args': place + '的現在時間是' + tzTime,
+                    })
                 return
-        print('not found')        
+        self.threadHandler.add_thread({
+            'class': 'class QuestionAnswering',
+            'func': 'class QuestionAnswering',
+            'args': place + '時間',
+            })  
 
+tz = WorldTime()
+tz.get_time_at_place('taipei')
