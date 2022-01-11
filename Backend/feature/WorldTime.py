46aee89a99e146dcabd30b38e6577c026518e3d2
Sophie8909 <sophielien09@gmail.com>
2022-01-11 21:30:49 +0800

    feat: worldTime.py add timeZone.json and run success

--- Backend/feature/WorldTime.py
+++ Backend/feature/WorldTime.py
@@ -1,6 +1,7 @@
 import pytz
 from datetime import datetime
 import sys
+import json
 try:
     import logger
     logger = logger.get_logger(__name__)
@@ -19,33 +20,42 @@ class WorldTime():
     def __init__(self):
         self.tzList = []
         self.threadHandler = None 
-    def import_thread(self, thread):
-		self.threadHandler = thread
-		return
-    def get_time_zone_list(self):
+    def import_thread(self, thread) -> None:
+        self.threadHandler = thread
+        return
+    def get_time_zone_list(self) -> None:
         if self.tzList == []:
-            self.tzList = pytz.all_timezones
-        return self.tzList
-    def get_time_at_place(self, place):
+            with open( 'feature/timeZone.json', encoding="utf-8") as f:
+                jsonContent = f.read()
+            jsonContent = json.loads(jsonContent)
+            # 取得 json "BODY" 欄位
+            self.tzList = jsonContent["timezoneList"]
+        return
+    def get_time_at_place(self, place) -> None:
         self.get_time_zone_list()
         # print(self.tzList)
         for tz in self.tzList:
-            if place.casefold() in tz.casefold():
-                tzId = pytz.timezone(tz)
+            if place.casefold() in tz["tzid"].casefold() or  place.casefold() in tz["cityNames"]:
+                tzId = pytz.timezone(tz["tzid"])
                 tzTime = datetime.now(tzId).strftime('%Y:%m:%d %H:%M:%S %Z %z')
-                logger.debug(tzTime)
+                sentence = place + '，現在時間是' + tzTime
+                logger.debug(sentence)
                 self.threadHandler.add_thread({
                     'class': 'TextToSpeech',
                     'func': 'text_to_voice',
-                    'args': place + '的現在時間是' + tzTime,
+                    'args': (sentence,)
                     })
                 return
         logger.debug('worldTime call QA')
         self.threadHandler.add_thread({
-            'class': 'class QuestionAnswering',
-            'func': 'class QuestionAnswering',
-            'args': place + '時間',
+            'class': 'QuestionAnswering',
+            'func': 'google_search',
+            'args': (place + '時間',)
             })  
+        return
 
-tz = WorldTime()
-tz.get_time_at_place('taipei')
+# if __name__ == '__main__':
+#     tz = WorldTime()
+#     while True:
+#         place = input("輸入地區：")
+#         tz.get_time_at_place(place)
