cc80d225c073df48ba1265718c64cd101660a255
Sophie8909 <sophielien09@gmail.com>
2022-01-17 01:11:44 +0800

    feat: support music and system volume switch

--- Backend/feature/Volume.py
+++ Backend/feature/Volume.py
@@ -17,16 +17,32 @@ except ModuleNotFoundError:
     logger.addHandler(ch)
 
 
-playing = True  # for Development
 
 
 class Volume:
     def __init__(self) -> None:
         self.systemVolume = 50
-        self.musicVolume = 50
-        return
+        self.musicVolume = 50 
+        self.threadHandler = None   
+        self.isPlaying = False
+    def import_thread(self, thread):
+		self.threadHandler = thread
+		return
     # alsa setting
-
+    def main(self):
+        while True:
+            bluetooth = self.threadHandler.get_instance("Bluetooth")
+            music = self.threadHandler.get_instance("MusicStreaming")
+            last_status = self.isPlaying
+            if bluetooth.isPlaying != True or bluetooth.isPlaying != True:
+                self.isPlaying = False
+            else:
+                self.isPlaying = True
+            if self.isPlaying != last_status:
+                if self.isPlaying:
+                    self.set_amixer("music")
+                else:
+                    self.set_amixer("system")
     def set_amixer(self, mode):
         if mode == 'music':
             cmd = r"amixer -M set Master {}\%".format(self.musicVolume)
@@ -38,33 +54,32 @@ class Volume:
             returned_value = os.system(cmd)
             logger.debug("change systemVolume")
             logger.debug(returned_value)
-
         return
 
     # unspecified
     def get_volume(self) -> int:
-        if playing:
+        if self.isPlaying:
             return self.get_music_volume()
         return self.get_system_volume()
 
     def set_volume(self, value=5):
         if value == None:
             value = 5
-        if playing:
+        if self.isPlaying:
             return self.set_music_volume(value)
         return self.set_system_volume(value)
 
     def louder_volume(self, value=5):
         if value == None:
             value = 5
-        if playing:
+        if self.isPlaying:
             return self.louder_music_volume(value)
         return self.louder_system_volume(value)
 
     def quieter_volume(self, value=5):
         if value == None:
             value = 5
-        if playing:
+        if self.isPlaying:
             return self.quieter_music_volume(value)
         return self.quieter_system_volume(value)
     # system
@@ -121,11 +136,12 @@ class Volume:
             self.set_amixer('music')
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
 #    def get_all_volume():
 #        print("System:", a.get_system_volume())
 #        print("Music:", a.get_music_volume())
-#    a = Volume()
+   a = Volume()
+   a.main()
 #    a.set_music_volume(50)
 #    get_all_volume()
 #    time.sleep(10)
